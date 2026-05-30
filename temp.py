d59d8a2b1dcfc3b1987da894edde490e0e44f506
nagaraj-trivedi <62987465+nagaraj-trivedi@users.noreply.github.com>
2026-05-30 11:58:43 +0530

    created the temp script

---
+++ temp.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+print"The script is invoked at \n")
+
+current_time = datetime.now()
+print(current_time)
+
+current_time = datetime.now().time()
+print(current_time)
